c96678dee45799239fa3ba4eb16e1cc6439d8ba5
kaylacoats <kayla.m.coats@gmail.com>
2021-10-07 15:37:43 -0500

    fixture rough draft - does not work

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,18 @@
 import pytest
-<<<<<<< HEAD
 from pmgr.project import Project, TaskException
 
-test = Project('clean')
+#test = Project('clean')
 
-def test_get_tasks():
-    assert test.get_tasks() == []
+test2 = Project('bake')
+
+#test.add_task('vacuum')
+
+#def test1():
+#     test1 = Project('clean')
+#     test1.add_task('vacuum')
+#     yield test1
+#     test1.remove_task('vacuum')
+
+def test_get_tasks(test1):
+    assert test1.get_tasks() == ['vacuum']
+    assert test2.get_tasks() == []
