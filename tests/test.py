395ace1946859fb2bc57b743c17ebce7635e131f
Jamie Wendt <jamie.wendt25@gmail.com>
2021-10-08 22:23:15 -0500

    added delete function to project.py

--- tests/test.py
+++ tests/test.py
@@ -1,17 +1,27 @@
 import pytest
-from pmgr.project import Project, TaskException 
+from pmgr.project import Project 
 
-@pytest.fixture (scope="function")
-def object_():
-    test1 = Project('clean')
+@pytest.fixture(scope="function")
+def testproj():
+    testproj = Project('clean')
+    yield testproj
+    #testproj.delete()
     
+
+def test_add_task(testproj):
+    #testproj.add_task("dust")
+    assert "dust" in testproj.get_tasks()
+
+
+
+
 
 
 #def test_add_task(self, task_name):
  #  assert add_task(test, sweep) == 'sweep'
 
-def test_get_tasks(object_):
-    assert object_.get_tasks() == ['vaccuum']
+#def test_get_tasks(object_):
+ #   assert object_.get_tasks() == ['vaccuum']
 
     #assert test2.get_tasks() == []
 
